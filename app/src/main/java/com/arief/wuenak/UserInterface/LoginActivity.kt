bf8bc525d9d29eb14147fa5c08c42d66bd974192
knight <ariefmaulana130598@gmail.com>
2020-03-04 21:55:59 +0700

    Initial commit

---
+++ app/src/main/java/com/arief/wuenak/UserInterface/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.arief.wuenak.UserInterface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arief.wuenak.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
