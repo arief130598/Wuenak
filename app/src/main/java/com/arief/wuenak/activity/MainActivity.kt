183c9348d30cef45d8a6b9ff5ea41925d7d38d63
knight <ariefmaulana130598@gmail.com>
2020-03-08 05:11:40 +0700

    master-stat

--- app/src/main/java/com/arief/wuenak/activity/MainActivity.kt
+++ app/src/main/java/com/arief/wuenak/activity/MainActivity.kt
@@ -2,12 +2,31 @@ package com.arief.wuenak.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.arief.wuenak.R
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.hello)
+        button.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.hello -> {
+                FirebaseAuth.getInstance().signOut()
+            }
+        }
+    }
+
+
 }
