eee7338f5ca6163d59820437e46e7d473530f029
knight <ariefmaulana130598@gmail.com>
2020-03-08 05:12:39 +0700

    Finish Login OATH

--- app/src/main/java/com/arief/wuenak/activity/LoginActivity.kt
+++ app/src/main/java/com/arief/wuenak/activity/LoginActivity.kt
@@ -1,25 +1,32 @@
 package com.arief.wuenak.activity
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.ActionMode
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.arief.wuenak.R
 import com.arief.wuenak.sharedpreference.IntroPreference
 import com.arief.wuenak.static.StaticCode.Companion.RC_SIGN_IN
 import com.arief.wuenak.ultility.ProgressBarUtility
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -28,7 +35,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var googleSignInOptions: GoogleSignInOptions
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var callbackManager: ActionMode.Callback2
+    private val callbackManager = CallbackManager.Factory.create();
 
     private val progressBar = ProgressBarUtility()
 
@@ -49,16 +56,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
         firebaseAuth = FirebaseAuth.getInstance()
         configureGoogleSignIn()
-
-
-    }
-
-    private fun configureGoogleSignIn() {
-        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        configureFacebookSignIn(this)
     }
 
     override fun onClick(v: View?) {
@@ -70,10 +68,91 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 startActivityForResult(signInIntent, RC_SIGN_IN)
             }
             R.id.login_facebook -> {
+                progressBar.show(this, "Please Wait...")
 
+                LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile","email"));
             }
         }
     }
+
+    private fun configureFacebookSignIn(context: Context) {
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    progressBar.dialog.dismiss()
+                    Toast.makeText(context, "Sign In Canceled", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: FacebookException) {
+                    progressBar.dialog.dismiss()
+                    Toast.makeText(context, "Sign In Failed", Toast.LENGTH_LONG).show()
+                }
+            })
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    progressBar.dialog.dismiss()
+
+                    goMain()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    progressBar.dialog.dismiss()
+                    try {
+                        throw task.exception!!
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                    } catch (e: Exception) {
+                        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+    }
+
+    private fun configureGoogleSignIn() {
+        googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                progressBar.dialog.dismiss()
+                goMain()
+                finish()
+            } else {
+                progressBar.dialog.dismiss()
+                try {
+                    throw it.exception!!
+                } catch (e: FirebaseAuthWeakPasswordException) {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                } catch (e: FirebaseAuthUserCollisionException) {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -89,30 +168,22 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 Toast.makeText(this, "Sign In Failed", Toast.LENGTH_LONG).show()
             }
         }
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val goMain = Intent(this@LoginActivity, MainActivity::class.java)
-                progressBar.dialog.dismiss()
-                startActivity(goMain)
-                finish()
-            } else {
-                progressBar.dialog.dismiss()
-                Toast.makeText(this, "Sign in failed", Toast.LENGTH_LONG).show()
-            }
-        }
+    fun goMain(){
+        val goMain = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(goMain)
+        finish()
     }
 
     override fun onStart() {
         super.onStart()
         val user = firebaseAuth.currentUser
         if (user != null) {
-            val goMain = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(goMain)
-            finish()
+            goMain()
         }
     }
 }
